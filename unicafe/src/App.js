b4a506faaa3c801b9de1a65486d1d7ab331a75df
Greg Valijan <greg.valijan@gmail.com>
2023-06-27 23:38:25 -0700

    Finished version of Unicafe

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,12 +1,41 @@
 import { useState } from 'react'
 
-const Display = props => <div>{props.value}</div>
-
 const Button = (props) => (
   <button onClick={props.handleClick}>
     {props.text}
   </button>
 )
+
+const Statistics = (props) => {
+  if (props.all == 0) {
+    return (
+      <div>
+        No Feedback Given
+      </div>
+    )
+  }
+  return(
+    <table>
+      <StatisticLine name="good" value = {props.good}/>
+      <StatisticLine name="neutral" value = {props.neutral}/>
+      <StatisticLine name="bad" value = {props.bad}/>
+      <StatisticLine name="all" value = {props.all}/>
+      <StatisticLine name="average" value = {props.average}/>
+      <StatisticLine name="positive" value = {props.positive+'%'}/>
+    </table>
+  )
+}
+
+const StatisticLine = ( { name, value } ) => {
+  return(
+      <tbody>
+        <tr>
+          <td>{name}</td>
+          <td>{value}</td>
+        </tr>
+      </tbody>
+  )
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -14,7 +43,9 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  let all = (good + bad + neutral);
+  let all = (good + bad + neutral)
+  let average = ((good - bad) / all).toFixed(1)
+  let positive = ((good/all) * 100).toFixed(1)
 
   return (
     <div>
@@ -23,12 +54,14 @@ const App = () => {
       <Button handleClick={() => setNeutral( neutral + 1 )} text="neutral" />
       <Button handleClick={() => setBad( bad + 1 )} text="bad" />
       <h1>Statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {(good - bad) / all}</p>
-      <p>postive {(good/all * 100)} %</p>
+      <Statistics
+        all = {all} 
+        good = {good}
+        neutral = {neutral}
+        bad = {bad}
+        average = {average}
+        positive = {positive}
+      />
     </div>
   )
 }
